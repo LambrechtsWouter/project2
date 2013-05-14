9368887b4c99bb6843e35d27288a23a1be7ad0e7
Wouter Lambrechts <wouter.lambrechts@gmail.com>
2013-05-14 03:27:08 -0500

    geolocation

--- photosearch/scripts/main.js
+++ photosearch/scripts/main.js
@@ -4,10 +4,47 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 // PhoneGap is ready
 function onDeviceReady() {
-    
-	
+    var watchID = navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 30000 });
+}
+function onSuccess(position) {
+    $('#text').empty();
+    $('#text').append('Latitude: '  + position.coords.latitude + '<br />' +
+                        'Longitude: ' + position.coords.longitude     + '<br />' +
+                        '<hr />');
+}
+function onError(error) {
+    alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
+function calculateDistances() {
+  var service = new google.maps.DistanceMatrixService();
+  service.getDistanceMatrix(
+    {
+      origins: origin1,
+      destinations: destinationA,
+      travelMode: google.maps.TravelMode.DRIVING,
+      unitSystem: google.maps.UnitSystem.METRIC,
+    }, callback);
 }
 
+function callback(response, status) {
+  if (status != google.maps.DistanceMatrixStatus.OK) {
+    alert('Error was: ' + status);
+  } else {
+    var origins = response.originAddresses;
+    var destinations = response.destinationAddresses;
+
+    for (var i = 0; i < origins.length; i++) {
+      var results = response.rows[i].elements;
+      for (var j = 0; j < results.length; j++) {
+
+        outputDiv.innerHTML += origins[i] + ' to ' + destinations[j]
+            + ': ' + results[j].distance.text + ' in '
+            + results[j].duration.text + '<br>';
+      }
+    }
+  }
+}
 var airlinesApp = function(){}
 
 airlinesApp.prototype = function() {
@@ -38,6 +75,7 @@ airlinesApp.prototype = function() {
             $.get('http://wouterlambrechts.ikdoeict.be/project2/api/Location/'+ route,function(data) {
                  console.log(data);
                 $('#streetview').append('<img src="http://maps.googleapis.com/maps/api/streetview?size=300x300&location=' + data.content[point].lat + ',' + data.content[point].lng + '&heading=151.78&pitch=-0.76&sensor=false">');
+                
             }, "json");
         });
         
@@ -118,6 +156,9 @@ airlinesApp.prototype = function() {
 			airData.getDataforFF(_ffNum,_handleDataForFF);
 		}
 	},
+   
+    
+	
     
     _handleDataForFF = function (data) {
         $flightList = $('#myTripsListView');	
@@ -131,7 +172,7 @@ airlinesApp.prototype = function() {
 	     		_customerData = data;
                 for (var i in data.content) {
                     var flight = data.content[i];
-                    $flightList.append('<li class="clearfix list" id="' + data.content[i].idRoutes + '"><img src="http://placehold.it/100x100"><a href="#tripDetail" data-transition="slide">'  + data.content[i].Name +  '</a><p>' +data.content[i].Duration + '</p></li>');
+                    $flightList.append('<li class="clearfix list" id="' + data.content[i].idRoutes + '"><img src="http://placehold.it/100x100"/><a href="#tripDetail" data-transition="slide">'  + data.content[i].Name +  '</a></li>');
 			        var item = $('#' +  data.content[i].idRoutes, $flightList);
                     item.data('flight', flight);
                     item.addClass('tripDetail');
